3195e619ec8a512f0e1ec0e7210905a2258b5824
ullasmo <ullasa.me18@bmsce.ac.in>
2023-02-07 15:34:20 +0530

    updates

--- client/script.js
+++ client/script.js
@@ -8,8 +8,12 @@ let loadInterval;
 
 function loader(element) {
   element.textContent = "";
+
   loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
     element.textContent += ".";
+
+    // If the loading indicator has reached three dots, reset it
     if (element.textContent === "....") {
       element.textContent = "";
     }
@@ -28,7 +32,11 @@ function typeText(element, text) {
     }
   }, 20);
 }
-function generateUniqueID() {
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16);
@@ -38,39 +46,45 @@ function generateUniqueID() {
 
 function chatStripe(isAi, value, uniqueId) {
   return `
-    <div class="wrapper ${isAi && "ai"}">
-      <div class="chat">
-       <div class="profile">
-          <img 
-            src="${isAi ? bot : user}" 
-            alt="${isAi ? "bot" : "user"}"
-          />
-       </div>
-       <div class="message" id=${uniqueId}>${value}</div>
-      </div>
-    </div>
-   `;
+        <div class="wrapper ${isAi && "ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? "bot" : "user"}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `;
 }
+
 const handleSubmit = async (e) => {
   e.preventDefault();
 
   const data = new FormData(form);
 
-  //user's chatstripe
+  // user's chatstripe
   chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  // to clear the textarea input
   form.reset();
 
-  //bot's chatstripe
-
-  const uniqueId = generateUniqueID();
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // specific message div
   const messageDiv = document.getElementById(uniqueId);
+
+  // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  // fetch data from server
-
-  const response = await fetch("https://codex-g13f.onrender.com/#", {
+  const response = await fetch("https://codex-g13f.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -81,18 +95,21 @@ const handleSubmit = async (e) => {
   });
 
   clearInterval(loadInterval);
-  messageDiv.innerHTML = "";
+  messageDiv.innerHTML = " ";
+
   if (response.ok) {
     const data = await response.json();
-    const parsedData = data.bot.trim();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
 
     typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
+
     messageDiv.innerHTML = "Something went wrong";
     alert(err);
   }
 };
+
 form.addEventListener("submit", handleSubmit);
 form.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
