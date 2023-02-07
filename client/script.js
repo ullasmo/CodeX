747a044da243712bdec7820593e383ef344adaea
ullasmo <ullasa.me18@bmsce.ac.in>
2023-02-07 11:39:49 +0530

    Update the server URL

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-g13f.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
